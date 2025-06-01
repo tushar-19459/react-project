d4e00f08ffd4d53966bf46412a31dcae08955588
tushar-19459 <19459tusharraj@gmail.com>
2025-06-01 18:06:24 +0530

    did the slide again

--- image-slide-app/src/Components/ImageSlider.jsx
+++ image-slide-app/src/Components/ImageSlider.jsx
@@ -1,19 +1,84 @@
 import { useEffect, useState } from "react"
 import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from 'react-icons/bs';
 
-export default function ImageSlider({ url, page, limit }) {
-    const [images, setImg] = useState([])
-    const [currentSlide, setCurrentSlide] = useState(0)
-    const [error, setError] = useState(null)
-    const [loading, setLoading] = useState(false)
+// export default function ImageSlider({ url, page, limit }) {
+//     const [images, setImg] = useState([])
+//     const [currentSlide, setCurrentSlide] = useState(0)
+//     const [error, setError] = useState(null)
+//     const [loading, setLoading] = useState(false)
 
-    async function fetchimages(url) {
+//     async function fetchimages(url) {
+//         try {
+//             setLoading(true)
+//             const responce = await fetch(url)
+//             const data = await responce.json()
+//             console.log(data)
+//             setImg(data)
+
+//         } catch (error) {
+//             setError(error)
+//         }
+//         finally {
+//             setLoading(false)
+//         }
+//     }
+
+//     useEffect(() => {
+//         console.log("useeffect")
+//         if (url !== '') {
+//             fetchimages(`${url}?page=${page}&limit=${limit}`)
+//         }
+//     }, [url])
+
+
+//     function handlenext(params) {
+//         setCurrentSlide(currentSlide === 0 ? images.length - 1 : currentSlide - 1)
+//     }
+//     function handleprev(params) {
+//         setCurrentSlide(currentSlide === images.length - 1 ? 0 : currentSlide + 1)
+//     }
+
+
+//     if (loading)
+//         return <div><h1>Loading...</h1></div>
+
+//     if (error !== null)
+//         return <div><h1>error:{error}</h1></div>
+
+//     return (
+//         <div className="main">
+//             <BsArrowLeftCircleFill onClick={handlenext} className="arrow leftButton"></BsArrowLeftCircleFill>
+//             <div className="image-div">
+//                 {images.length > 0 && (images.map((img, index) =>
+//                     <img key={index} src={img.download_url} className={currentSlide === index ? "current-image" : "current-image hide-current-image"} alt="" />
+//                 ))}
+//             </div>
+//             <BsArrowRightCircleFill onClick={handleprev} className="arrow rightButton"></BsArrowRightCircleFill>
+//             <div className="circle-indicator">
+//                 <span className="indicator">
+//                     {images.map((_, index) =>
+//                         <button onClick={() => setCurrentSlide(index)} className={currentSlide === index ? "indicator-button active" : "indicator-buttons"} key={index}></button>
+//                     )}
+//                 </span>
+//             </div>
+//         </div>
+//     )
+// }
+
+export default function ImageSlider({ url, page, limit }) {
+
+    const [loading, setLoading] = useState(false)
+    const [slide, setSlide] = useState(0)
+    const [error, setError] = useState(null)
+    const [images, setImages] = useState([])
+
+    async function apicall(url) {
         try {
             setLoading(true)
             const responce = await fetch(url)
             const data = await responce.json()
             console.log(data)
-            setImg(data)
+            setImages(data)
 
         } catch (error) {
             setError(error)
@@ -24,45 +89,46 @@ export default function ImageSlider({ url, page, limit }) {
     }
 
     useEffect(() => {
-        console.log("useeffect")
-        if (url !== '') {
-            fetchimages(`${url}?page=${page}&limit=${limit}`)
-        }
+        if (url)
+            apicall(`${url}?page=${page}&limit=${limit}`)
     }, [url])
 
 
-    function handlenext(params) {
-        setCurrentSlide(currentSlide === 0 ? images.length - 1 : currentSlide - 1)
+    if (error) {
+        return (
+            <div>Error:{error}</div>
+        )
     }
-    function handleprev(params) {
-        setCurrentSlide(currentSlide === images.length - 1 ? 0 : currentSlide + 1)
+    if (loading) {
+        return (
+            <h1>Loading...</h1>
+        )
     }
 
-
-    if (loading)
-        return <div><h1>Loading...</h1></div>
-
-    if (error !== null)
-        return <div><h1>error:{error}</h1></div>
+    function preimg(params) {
+        console.log("pre "+slide)
+        setSlide(slide === 0 ? images.length - 1 : slide - 1)
+    }
+    function nextimg(params) {
+        console.log("next "+slide)
+        setSlide(slide === images.length - 1 ? 0 : slide + 1)
+    }
 
     return (
 
-        <div className="main">
-            <BsArrowLeftCircleFill onClick={handlenext} className="arrow leftButton"></BsArrowLeftCircleFill>
-            <div className="image-div">
-                {images.length > 0 && (images.map((img, index) =>
-                    <img key={index} src={img.download_url} className={currentSlide === index ? "current-image" : "current-image hide-current-image"} alt="" />
-                ))}
-            </div>
-            <BsArrowRightCircleFill onClick={handleprev} className="arrow rightButton"></BsArrowRightCircleFill>
-            <div className="circle-indicator">
-                <span className="indicator">
-                    {images.map((_, index) =>
-                        <button onClick={() => setCurrentSlide(index)} className={currentSlide === index ? "indicator-button active" : "indicator-buttons hide-indicator-buttons"} key={index}></button>
+            <div className="main">
+                <BsArrowLeftCircleFill className="arrow left-button" onClick={preimg}></BsArrowLeftCircleFill>
+                <div className="image-div">
+                    {images.map((data, index) =>
+                        <img src={data.download_url} className={slide===index?'current-image':'current-image hide'} key={index} alt="" />
                     )}
-                </span>
+                </div>
+                <BsArrowRightCircleFill className=" arrow right-button" onClick={nextimg}></BsArrowRightCircleFill>
+                <div className="indicator">
+                    {images.map((_, index) =>
+                        <button key={index} onClick={()=>setSlide(index)} className={slide===index?"bottom-button active":"bottom-button "}  ></button>
+                    )}
+                </div>
             </div>
-        </div>
-
     )
 }
